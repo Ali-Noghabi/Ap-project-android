e74d35abafa18df75036a05841c3631ac49fe003
LAPTOP-396CDD4N\moons <miryamsadeghi82@gmail.com>
2021-07-12 16:59:47 +1000

    design search view page

--- app/src/main/java/ir/lucifer/approject/SearchViewPage.java
+++ app/src/main/java/ir/lucifer/approject/SearchViewPage.java
@@ -13,16 +13,16 @@ public class SearchViewPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_view_page);
 
-        categorySpinner = findViewById(R.id.categorySVP);
-        String array_spinner[] = new String[10];
-        array_spinner[0] = "CAR";
-        array_spinner[1] = "CARPET";
-        array_spinner[2] = "HOME";
-        array_spinner[3] = "PC";
-        array_spinner[4] = "LAPTOP";
-
-        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, array_spinner);
-        categorySpinner.setAdapter(adapter);
+//        categorySpinner = findViewById(R.id.categorySVP);
+//        String array_spinner[] = new String[10];
+//        array_spinner[0] = "CAR";
+//        array_spinner[1] = "CARPET";
+//        array_spinner[2] = "HOME";
+//        array_spinner[3] = "PC";
+//        array_spinner[4] = "LAPTOP";
+//
+//        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, array_spinner);
+//        categorySpinner.setAdapter(adapter);
 
 
     }
