b10e53a679071646a7f26075b8faca35af0573cd
ali.noghabi <a.noghabi2002@gmail.com>
2021-07-17 22:24:26 +0430

    idk XD

--- app/src/main/java/ir/lucifer/approject/MainActivity.java
+++ app/src/main/java/ir/lucifer/approject/MainActivity.java
@@ -102,34 +102,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
- /*   private void ShowProducts() {
-        Call<List<Product>> call = mainAPI.getProductList();
-
-        //gettt
-        call.enqueue(new Callback<List<Product>>() {
-            @Override
-            public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
-                if (response.isSuccessful()) {
-                    List<Product> products = response.body();
-                    for (Product tempPro : products) {
-                        String ret = "";
-                        ret += "Subject : " + tempPro.subject + "\n";
-                        ret += "Description" + tempPro.description + "\n";
-                        ret += "Price : " + tempPro.price + "\n\n";
-//                        test.append(ret);
-                    }
-
-                } else {
-//                    test.setText("Error " + response.code());
-                }
-
-            }
-
-            @Override
-            public void onFailure(Call<List<Product>> call, Throwable t) {
-//                test.setText(t.getMessage());
-            }
-        });
-    }
-*/
 }
